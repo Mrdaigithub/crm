e9780c58e8c0c9f77e9350e5f9797ac594fe73a1
Mrdai <346139312@qq.com>
2017-04-25 13:42:30 +0800

    update menu.php

--- back_end/api/v1/menu/index.php
+++ back_end/api/v1/menu/index.php
@@ -6,27 +6,24 @@ include '../../../core/core.php';
 //获取用户对应的菜单数据
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-    $permission->has_permission('patient');
+    $menu_data = $sql->query("SELECT id,title,name,url FROM menu");
+    $menu_json = array();
 
+//    构建父菜单
+    foreach ($menu_data as $item) {
+        if (strlen($item['name']) === 1) {
+            if (!$permission->has_permission('index/home') && $item['name'] === '1') continue; //无权查看控制台
+            array_push($menu_json, array(
+                'title' => $item['title'],
+                'name' => $item['name'],
+                'url' => $item['url'],
+                'child' => array()
+            ));
+        }
+    }
 
-////    $power = $sql->query("SELECT power FROM users WHERE username='" . $username . "';");
-//    $permission_data = $sql->query("SELECT permission FROM role WHERE id=(SELECT id FROM users WHERE username='$username');");
-//    $permission_data = $permission_data[0]['permission'];
-//
-////    $power = $power[0]['power'];
-//    $menu_data = $sql->query("SELECT id,title,name,url FROM menu");
-//    $menu_json = array();
-//    foreach ($menu_data as $item) {
-//        if (strlen($item['name']) === 1) {
-//            array_push($menu_json, array(
-//                'title' => $item['title'],
-//                'name' => $item['name'],
-//                'url' => $item['url'],
-//                'child' => array()
-//            ));
-//        }
-//    }
-//    foreach ($menu_data as $item) {
+//    构建子菜单
+    foreach ($menu_data as $item) {
 //        if (preg_match('/\d-\d+/', $item['name'])) {
 //            $rootName = $item['name'][0];
 //            for ($i = 0; $i < count($menu_json); $i++) {
@@ -35,32 +32,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 //                }
 //            }
 //        }
-//    }
+    }
 
+//    if (!$permission->has_permission('index/home')) $menu_json = array_splice($menu_json,0,1);
+//
+//    if (!$permission->has_permission('patient')) array_splice($menu_json,1,1); //无权查看客户信息
+//
+//    if (!$permission->has_permission('data')) array_splice($menu_json,2,1); //无权查看数据报表
+//    else{
+//        if (!$permission->has_permission('t_report_data')) array_shift($menu_json[0]['child']); //无权查看绩效数据(文本)
+//        if (!$permission->has_permission('c_report_data')) array_pop($menu_json[0]['child']); //无权查看绩效数据(图形)
+//    }
+//
+//    if (!$permission->has_permission('info')) array_shift($menu_json); //无权查看信息管理
 
-////    无权查看控制台
-//    if ($permission[1] === '0') array_shift($menu_json);
-//
-////    无权查看客户信息
-//    if ($permission[3] === '0') array_shift($menu_json);
-//
-////    无权查看数据报表
-//    if ($permission[46] === '0') array_shift($menu_json);
-//    else{
-////        无权查看绩效数据(文本)
-//        if ($permission[51] === '0') array_shift($menu_json[0]['child']);
-////        无权查看绩效数据(图形)
-//        if ($permission[55] === '0') array_pop($menu_json[0]['child']);
-//    }
-//
-////    无权查看信息管理
-//    if ($permission[29] === '0') array_shift($menu_json);
-//    else{
-////        无权查看信息管理
-//        if ($permission[30] === '0') array_shift($menu_json);
-//    }
 //    print_r($menu_json);
-//    echo json_encode(array('menu_data'=>$menu_json));
     exit();
 }
 error_handler(43003);
