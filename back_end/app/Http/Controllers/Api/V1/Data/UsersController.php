dae8d1aa9642f1e96e1bbc39308034b4a8d9b666
Mrdai <346139312@qq.com>
2017-07-24 14:14:08 +0800

    update .gitignore

--- back_end/app/Http/Controllers/Api/V1/Data/UsersController.php
+++ back_end/app/Http/Controllers/Api/V1/Data/UsersController.php
@@ -49,84 +49,66 @@ class UsersController extends Controller
      */
     public function index()
     {
-        return User::where('id', 6)->with(['patient'=>function($query){
-            $query->select('id', 'name', 'state', 'created_at', 'arrive_date');
-            $query->with(['channel'=>function($c_query){
-                return $c_query->where('id', 3);
-            }]);
-            return $query->where('state', 2);
-        }])->get();
-//        $parameters = Input::all();
-//        $users = User::all();
-//        $res = [];
-//        $date_type = $parameters['date_type'] === 'created' ? 'created_at' : 'arrive_date';
-//        if ($parameters['statistical_type'] === 'day' &&
-//            date_diff(date_create($parameters['start_date']), date_create($parameters['end_date']))->m >= 1) {
-//            // 按日计算, 范围超过一个月就按一个月算, 根据end_date修改start_date
-//            $parameters['start_date'] = date('Y-m-d', strtotime($parameters['end_date'] . " -1 Month"));
-//        }
-//        if ($parameters['statistical_type'] === 'year') {
-//            $start_date_year = date('Y', strtotime($parameters['start_date']));
-//            $end_date_year = (int)date('Y', strtotime($parameters['end_date']));
-//            for ($year = $end_date_year; $year >= $start_date_year; --$year) {
-//                $item['date'] = $year;
-//                $item['data'] = [];
-//                foreach ($users as $user) {
-//                    array_push($item['data'], $user);
-//                }
-//                array_push($res, $item);
-//            }
-//        }
-//        return $res;
+        $parameters = Input::all();
+        $users = User::all();
+        $dates = $this->createDateRange($parameters['statistical_type'], $parameters['start_date'], $parameters['end_date']);
 
-
-
-//        $patients = Patient::whereBetween($date_type, [$parameters['start_date'], $parameters['end_date']])->get();
-//        if (key_exists('channel', $parameters)) {
-//            $arr = [];
-//            foreach ($patients as $patient) {
-//                $patient->channel;
-//                if ($patient->channel[0]->id === (int)$parameters['channel']) {
-//                    array_push($arr, $patient);
-//                }
-//            }
-//            $patients = $arr;
-//        }
-//        if (key_exists('state', $parameters)) {
-//            $arr = [];
-//            foreach ($patients as $patient) {
-//                if ($patient->state === (int)$parameters['state']) {
-//                    array_push($arr, $patient);
-//                }
-//            }
-//            $patients = $arr;
-//        }
-//        if ($parameters['statistical_type'] === 'year') {
-//            $state_date_year = date('Y', strtotime($parameters['start_date']));
-//            $end_date_year = (int)date('Y', strtotime($parameters['end_date']));
-//            for ($year=$end_date_year; $year>=$state_date_year; --$year) {
-//                foreach ()
-//            }
-//        }
-//        return $patients;
+        foreach ($users as $user) {
+            $x = $user->with(['patient' => function ($query) use ($parameters) {
+//                $query->select('id', 'name', 'state', 'created_at', 'arrive_date');
+//                $query->with(['channel' => function ($c_query) use ($parameters) {
+//                    return $c_query->where('id', $parameters['channel']);
+//                }]);
+                return $query->where('state', 2)
+                    ->whereBetween($parameters['date_type'], ['2016-04-08', '2017-7-24']);
+            }])->get();
+            print_r(json_encode($x));
+        }
     }
 
-    private function deal($patients, $date)
+    /**
+     * 创建日期范围
+     *
+     * @param $statistical_type
+     * @param $_start_date
+     * @param $_end_date
+     * @return array
+     */
+    private function createDateRange($statistical_type = 'month', $_start_date, $_end_date)
     {
-        $item['date'] = (string)$date;
-        if ($patients === []) {
-            $item['advisory'] = 0;
-            $item['arrive'] = 0;
-            $item['lose'] = 0;
-        } else {
-            $item['advisory'] = count($patients);
-            $item['arrive'] = 0;
-            $item['lose'] = 0;
-            foreach ($patients as $patient) {
-                if ($patient->state === 2) $item['arrive']++;
-                if ($patient->state === 3) $item['lose']++;
+        $dates = [];
+        if ($statistical_type === 'year') {
+            $start_date = (int)date('Y', strtotime($_start_date));
+            $end_date = (int)date('Y', strtotime($_end_date));
+            for ($year = $start_date; $year <= $end_date; $year++) {
+                if ($year === $start_date) {
+                    array_push($dates, [$_start_date, $year . '-12-31']);
+                    continue;
+                }
+                if ($year === $end_date) {
+                    array_push($dates, [$year . '-01-01', $_end_date]);
+                    continue;
+                }
+                array_push($dates, [$year . '-01-01', $year . '-12-31']);
+            }
+        } elseif ($statistical_type === 'month') {
+            $_start_date = date('Y-m', strtotime($_start_date));
+            $_end_date = date('Y-m', strtotime($_end_date));
+            $month_diff = date_diff(date_create($_start_date), date_create($_end_date));
+            $sum_month = $month_diff->y * 12 + $month_diff->m;
+            for ($i = 0, $j = 1; $i <= $sum_month; $i++, $j++) {
+                array_push($dates, [date('Y-m', strtotime("$_start_date+$i Month")), date('Y-m', strtotime("$_start_date+$j Month"))]);
+            }
+        } elseif ($statistical_type === 'day') {
+            $start_date = date('Y-m-d', strtotime($_start_date));
+            $end_date = date('Y-m-d', strtotime($_end_date));
+            $date_diff = date_diff(date_create($_start_date), date_create($_end_date));
+            if ($date_diff->m <= 0) {
+                for ($i = 0, $j = 1; $i <= $date_diff->d; $i++, $j++) {
+                    array_push($dates, [date("Y-m-d", strtotime("$start_date+$i Day")), date("Y-m-d", strtotime("$start_date+$j Day"))]);
+                }
             }
         }
-        return $item;
+        return $dates;
     }
 }
