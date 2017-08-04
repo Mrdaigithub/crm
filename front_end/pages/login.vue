62cee656250c773f04263ffa30369314a8947b9b
Mrdaigithub <346139312@qq.com>
2017-08-04 23:01:53 +0800

    美化登陆界面

--- front_end/pages/login.vue
+++ front_end/pages/login.vue
@@ -2,16 +2,16 @@
   <div class="login">
     <el-row type="flex" justify="center">
       <el-col :span="8" class="login-wrap">
-        <h2>Crm</h2>
-        <h3>Login</h3>
+        <h2><i class="el-icon-ali-logo"></i></h2>
+        <h3>登录</h3>
         <el-form :model="loginFrom" :rules="loginRules" ref="loginFrom">
           <el-form-item prop="username">
             <el-input type="text" v-model="loginFrom.username" auto-complete="off" v-focus="true" icon="ali-account"
-                      placeholder="username"></el-input>
+                      placeholder="用户名"></el-input>
           </el-form-item>
           <el-form-item prop="password">
             <el-input type="password" v-model="loginFrom.password" auto-complete="off" icon="ali-lock"
-                      @keyup.native.enter="login('loginFrom')" placeholder="password"></el-input>
+                      @keyup.native.enter="login('loginFrom')" placeholder="密码"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button class="login-btn" type="primary" @click="login('loginFrom')" :disabled="disabled">login
@@ -37,12 +37,14 @@
         },
         loginRules: {
           username: [
-            {required: true, message: 'username?', trigger: 'blur'},
-            {min: 4, max: 15, message: 'length is 4-15', trigger: 'blur'}
+            {required: true, message: '用户名不能为空', trigger: 'blur'},
+            {min: 4, message: '用户名过短', trigger: 'blur'},
+            {max: 15, message: '用户名过长', trigger: 'blur'}
           ],
           password: [
-            {required: true, message: 'password?', trigger: 'blur'},
-            {min: 4, max: 15, message: 'length is 4-15', trigger: 'blur'}
+            {required: true, message: '密码不能为空', trigger: 'blur'},
+            {min: 4, message: '密码过短', trigger: 'blur'},
+            {max: 15, message: '密码过长', trigger: 'blur'}
           ]
         }
       }
@@ -64,7 +66,7 @@
               }))).token
               if (token) {
                 self.$message.success({
-                  message: 'login success',
+                  message: '登陆成功',
                   duration: 800,
                   onClose: function () {
                     sessionStorage.token = token
@@ -102,24 +104,22 @@
       margin-top: 25vh;
       background-color: #fff;
       h2 {
+        text-align: center;
         font-weight: normal;
         color: #7d7d7d;
         line-height: 40px;
-        font-size: 30px;
+        font-size: 66px;
         margin: 10px 20px 0 20px;
-        text-indent: 45px;
-        background: url("/images/logo.png") no-repeat left center;
-        background-size: contain;
       }
       h3 {
-        font-weight: normal;
+        font-weight: 100;
         color: #7d7d7d;
         line-height: 35px;
-        font-size: 40px;
-        margin: 20px 20px 0 20px;
+        font-size: 34px;
+        margin: 5px 20px 0 20px;
       }
       .el-form {
-        margin: 30px;
+        margin: 25px 30px 45px 30px;
         .login-btn {
           width: 100%;
         }
