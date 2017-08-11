d3ea2a6bf6036da94bcf1816ab8b5ba747488d30
Mrdaigithub <346139312@qq.com>
2017-08-11 23:15:21 +0800

    系统管理模块添加permission控制

--- back_end/app/Http/Controllers/Api/V1/LogsController.php
+++ back_end/app/Http/Controllers/Api/V1/LogsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Log;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use JWTAuth, JWTException;
 
 class LogsController extends Controller
 {
@@ -15,6 +16,9 @@ class LogsController extends Controller
      */
     public function index()
     {
+        if (!JWTAuth::parseToken()->authenticate()->roles[0]->hasPermission('allow_setting_module')) $this->response->errorForbidden(403033);
+        if (!JWTAuth::parseToken()->authenticate()->roles[0]->hasPermission('setting/log')) $this->response->errorForbidden(403034);
+        
         return Log::paginate(20);
     }
 }
